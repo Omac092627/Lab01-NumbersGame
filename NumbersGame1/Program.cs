cf4ffd0c6fb1b13e6decd9d8703609e5e4a62b73
Olamide Ogunde <oogunde0413@gmail.com>
2020-07-06 17:01:11 -0700

    I added the array list to the app.

--- NumbersGame1/Program.cs
+++ NumbersGame1/Program.cs
@@ -29,10 +29,47 @@ namespace NumbersGame1
 
         static void StartSequence()
         {
+
             Console.WriteLine("Please pick a number greater than 0: ");
             Convert.ToInt32(Console.ReadLine());
 
-            int[] numberEntered = new int[5];
+            int[] numberEntered = new int[6];
+
+
+            Populate(numberEntered);
         }
+
+ 
+
+        static int[] Populate(int[] int1)
+            {
+            int[] numberEntered = new int[6];
+            int count = 0;   
+
+         for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Please enter a random number between {count} and 6: ");
+                numberEntered[i] = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+
+         for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"You entered {numberEntered[i]}");
+            }
+
+            Console.WriteLine("You array size is 6");
+            Console.ReadLine();
+
+            return int1;
+
+
+        }
+
+
+        /*static int GetSum(int int2)
+        {
+
+        }*/
     }
 }
